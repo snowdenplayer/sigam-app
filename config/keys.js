c81c68e08b332c3e99ac9a4a7591f19a7efaf28f
snowden <rebitson20@gmail.com>
2021-04-05 13:15:38 +0300

    production database

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,12 @@
-module.exports = {
-    mongoURI: "mongodb://localhost:27017/meven_auth",
-    secret: 'yoursecret'
-};
+if(process.env.NODE_ENV === 'production'){
+    module.exports = {
+        mongoURI: "mongodb+srv://root:root@cluster0.ejiy0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+        secret: 'yoursecret'
+    };
+}else{
+    module.exports = {
+        mongoURI: "mongodb://localhost:27017/meven_auth",
+        secret: 'yoursecret'
+    };
+}
+
